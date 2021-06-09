324695c0db71904ad9547c34de09eaf6907cb149
katruya <kybryce4@gmail.com>
2021-06-09 14:25:40 -0500

    deleted calculate functions - redundant, unused

--- calculator.js
+++
@@ -1,8 +0,0 @@
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const divide = (a, b) => a / b;
-const multiply = (a, b) => a * b;
-const calculate = (func, a, b) => func(a, b);
-// console.log(calculate(add, 2, 4));
-export default { calculate, add, subtract, divide, multiply };
-//# sourceMappingURL=calculator.js.map
