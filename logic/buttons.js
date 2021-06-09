ef7e1384cc90a7772612bf2fa9121ba39667fa9f
katruya <kybryce4@gmail.com>
2021-06-09 09:54:19 -0500

    buttons now store/display numbers clicked upon in app

--- logic/buttons.js
+++ logic/buttons.js
@@ -15,6 +15,10 @@ let nine = document.getElementById('nine');
 // input area
 let calculatorIO = document.getElementById('calculator-io');
 // number button events
+zero.addEventListener("click", () => {
+    calculatorIO.innerHTML += `<h1>0</h1>`;
+    Memory.push('0');
+});
 one.addEventListener("click", () => {
     // store 1 in input area
     calculatorIO.innerHTML += `<h1>1</h1>`;
@@ -28,6 +32,30 @@ two.addEventListener("click", () => {
 three.addEventListener("click", () => {
     calculatorIO.innerHTML += `<h1>3</h1>`;
     Memory.push('3');
+});
+four.addEventListener("click", () => {
+    calculatorIO.innerHTML += `<h1>4</h1>`;
+    Memory.push('4');
+});
+five.addEventListener("click", () => {
+    calculatorIO.innerHTML += `<h1>5</h1>`;
+    Memory.push('5');
+});
+six.addEventListener("click", () => {
+    calculatorIO.innerHTML += `<h1>6</h1>`;
+    Memory.push('6');
+});
+seven.addEventListener("click", () => {
+    calculatorIO.innerHTML += `<h1>7</h1>`;
+    Memory.push('7');
+});
+eight.addEventListener("click", () => {
+    calculatorIO.innerHTML += `<h1>8</h1>`;
+    Memory.push('8');
+});
+nine.addEventListener("click", () => {
+    calculatorIO.innerHTML += `<h1>9</h1>`;
+    Memory.push('9');
 });
 export default {};
 //# sourceMappingURL=buttons.js.map
